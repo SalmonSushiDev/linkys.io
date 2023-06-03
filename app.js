0b701e0f119f3937fb34baf898379a20ea659b41
SushiSalmonDev <108323624+SalmonSushiDev@users.noreply.github.com>
2023-06-03 13:49:18 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+//for links//
+let da = document.getElementById('da');
+let scr = document.getElementById('sc');
+let twit = document.getElementById('tw');
+let ibis = document.getElementById('ib');
+let dis = document.getElementById('di');
+let git = document.getElementById('gi');
+let cdp = document.getElementById('co');
+let rob = document.getElementById('ro');
+
+da.addEventListener('click', function(){
+    window.open('https://www.deviantart.com/5h3rapr', 'target_blank')
+});
+scr.addEventListener('click', function(){
+    window.open('https://scratch.mit.edu/users/AprillTest/', 'target_blank')
+});
+twit.addEventListener('click', function(){
+    window.open('https://twitter.com/5h3rApr', 'target_blank')
+});
+ibis.addEventListener('click', function(){
+    window.open('https://ibispaint.com/artist1/1651487144354119680/', 'target_blank')
+});
+dis.addEventListener('click', function(){
+    alert('5h3rApr#6097');
+});
+git.addEventListener('click', function(){
+    window.open('https://github.com/SalmonSushiDev', 'target_blank')
+});
+cdp.addEventListener('click', function(){
+    window.open('https://codepen.io/SalmonSushiDev', 'target_blank')
+});
+rob.addEventListener('click', function(){
+    window.open('https://www.roblox.com/users/3393786005/profile', 'target_blank')
+});
+
+//This was all made by me ^^
+//Inspired by carrd.co! :D
